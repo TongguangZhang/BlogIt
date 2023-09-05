19a84fb38055a74b5804ae9ad5a3ca650d4564b4
TongguangZhang <tonggz250@gmail.com>
2023-09-05 18:19:44 -0400

    get, post, and delete requests

--- app.js
+++ app.js
@@ -24,16 +24,21 @@ app.set("view engine", "ejs");
 // middleware and static files
 app.use(morgan("tiny")); // logs requests
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
 // routes
+
+// homepage, redirects to blogs
 app.get("/", (req, res) => {
     res.redirect("/blogs");
 });
 
+// about page
 app.get("/about", (req, res) => {
     res.render("about", { title: "About" });
 });
 
+// blogs
 app.get("/blogs", (req, res) => {
     Blog.find()
         .sort({ createdAt: -1 })
@@ -43,9 +48,41 @@ app.get("/blogs", (req, res) => {
         .catch((err) => console.log(err));
 });
 
-// new blog
+// new blog form
 app.get("/blogs/create", (req, res) => {
     res.render("create", { title: "Create" });
+});
+
+// single blog details
+app.get("/blogs/:id", (req, res) => {
+    const id = req.params.id;
+    Blog.findById(id)
+        .then((result) => {
+            res.render("details", { blog: result, title: "Blog Details" });
+        })
+        .catch((err) => console.log(err));
+});
+
+// creating new blog
+app.post("/blogs", (req, res) => {
+    const blog = new Blog(req.body);
+
+    blog.save()
+        .then((result) => {
+            res.redirect("/blogs");
+        })
+        .catch((err) => console.log(err));
+});
+
+// deleting blog
+app.delete("/blogs/:id", (req, res) => {
+    const id = req.params.id;
+
+    Blog.findByIdAndDelete(id)
+        .then((result) => {
+            res.json({ redirect: "/blogs" });
+        })
+        .catch((err) => console.log(err));
 });
 
 // 404 (default) must be at bottom - triggers for every request
